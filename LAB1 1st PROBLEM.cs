ebbdc1c3c3f23fd9b2dcc11fad77496869788291
pandora25 <mcyalda@yahoo.com>
2018-03-08 14:09:51 -0500

    lab one with 1st & 2nd problems

---
+++ LAB1 1st PROBLEM.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool canContinue = true;
+
+            Console.WriteLine("Please enter a number");
+            string userInputOne = Console.ReadLine();
+
+            Console.WriteLine("Please enter a second number");
+            string userInputSecond = Console.ReadLine();
+
+            int result1;
+            bool canParse1 = Int32.TryParse(userInputOne, out result1);
+
+            int result2;
+            bool canParse2 = Int32.TryParse(userInputSecond, out result2);
+
+            if (canParse1 == false || canParse2 == false)
+            {
+                canContinue = false;
+            }
+
+            if (canContinue)
+            {
+
+                char[] firstCharArray = userInputOne.ToCharArray();
+                char[] secondCharArray = userInputSecond.ToCharArray();
+
+                int[] firstSequence = firstCharArray.Select(c => Convert.ToInt32(c.ToString())).ToArray();
+                int[] secondSequence = secondCharArray.Select(c => Convert.ToInt32(c.ToString())).ToArray();
+
+
+                if (firstSequence.Length != secondSequence.Length)
+                {
+                    canContinue = false;
+                }
+
+                if (canContinue)
+                {
+                    int sum1 = 0;
+                    int sum2 = 0;
+
+                    for (int index1 = 0; index1 < firstSequence.Length; index1++)
+                    {
+                        for (int index2 = 0; index2 < secondSequence.Length; index2++)
+                        {
+                            if (sum1 == 0)
+                            {
+                                if (index1 == index2)
+                                {
+                                    sum1 = firstSequence[index1] + secondSequence[index2];
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                if (index1 == index2)
+                                {
+                                    sum2 = firstSequence[index1] + secondSequence[index2];
+                                }
+                            }
+                        }
+                    }
+                    if (sum1 == sum2)
+                    {
+                        Console.WriteLine("YAY");
+                    }
+                    else
+                    {
+                        Console.Write("Kuss Ahmar");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("You did not enter the same number length");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You did not enter integers");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LAB1 2ND PROBLEM.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp25
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number in the following format - MM/DD/YYYY");
+
+            DateTime? firstDate = TryParseDate(Console.ReadLine());
+
+            Console.WriteLine("Please enter a 2nd number in the following format - MM/DD/YYYY");
+
+            DateTime? secondDate = TryParseDate(Console.ReadLine());
+
+            if (firstDate.HasValue && secondDate.HasValue)
+            {
+                TimeSpan difference = secondDate.Value - firstDate.Value;
+
+                Console.WriteLine("The result is " + Environment.NewLine + difference.TotalDays + " days " +
+                                  Environment.NewLine + difference.TotalHours + " hours " + Environment.NewLine +
+                                  difference.TotalMinutes + " minutes");
+            }
+            else
+            {
+                Console.WriteLine("You have not followed the instructions!");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static DateTime? TryParseDate(string dateEntered)
+        {
+            DateTime date;
+            if (DateTime.TryParse(dateEntered, out date))
+            {
+                return date;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
+
